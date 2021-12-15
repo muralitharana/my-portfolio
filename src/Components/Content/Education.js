c5143b0988c2b7dad7a277c9cee018a36506ce00
muralitharana <muralitharan0002@gmail.com>
2021-12-15 10:25:56 +0530

    education

--- src/Components/Content/Education.js
+++ src/Components/Content/Education.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import Header from '../Header/Header'
+import React from "react";
+import Header from "../Header/Header";
+import "./Education.css";
+import EducationBanner from "../../Image/Educations.jpg";
 const Education = () => {
-    return (
-        <div>
-            <Header />
-            Education
+  return (
+    <>
+      <Header />
+      <div className="educationContainer">
+        <div className="educationContent">
+          <div className="educationItems">
+            <ul>
+              <li className="standard">
+                Bachelor of Science in Electronics (77%)
+              </li>
+              <li className="collageName">
+                PSG College Of Arts &#38; Science{" "}
+              </li>
+            </ul>
+            <ul>
+              <li className="standard">
+                12<sup>th</sup> STD (89.91%)
+              </li>
+              <li className="collageName">
+                Laurel Higher Secondary School
+              </li>
+            </ul>
+            <ul>
+              <li className="standard">
+                10<sup>th</sup> STD (90.2%)
+              </li>
+              <li className="collageName">
+                Laurel Higher Secondary School
+              </li>
+            </ul>
+          </div>
+          
+          <div className="educationItems">
+            <img src={EducationBanner} alt="Eduaction image" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Education
+export default Education;
