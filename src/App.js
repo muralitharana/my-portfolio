9b68e4362e101e0d207df2c965e6b4d76cf8174d
muralitharana <muralitharan0002@gmail.com>
2021-12-16 14:05:13 +0530

    lazy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Header from "./Components/Header/Header";
 import RouterComp from "./Components/route/RouterComp";
-
+import { lazy, Suspense } from "react";
+import "./App.css";
+const LazyLoad = lazy(
+  () =>
+    new Promise((resolve, reject) =>
+      setTimeout(() => resolve(import("./Components/route/RouterComp")), 2000)
+    )
+);
 function App() {
   return (
     <div className="App">
-      <RouterComp/>
+      <Suspense
+        fallback={
+          <div className="lazyConatiner">
+            <div class="spinner-box">
+              <div class="circle-border">
+                <div class="circle-core"></div>
+              </div>
+              <p className="para">M</p>
+            </div>
+          </div>
+        }
+      >
+        <LazyLoad />
+      </Suspense>
     </div>
   );
 }
